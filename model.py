df4e818bffae93e3f194e7b6b0d807d82a33e504
Sree Harish <sreeharish.me@gmail.com>
2026-08-11 15:01:00 +0530

    Step 3: build_itos

--- model.py
+++ model.py
@@ -32,8 +32,14 @@ def build_stoi(vocab):
     return {ch: i for i, ch in enumerate(vocab)}
 '''
 
-# Step 3 - build_itos (not yet solved)
-# TODO: implement
+# Step 3 - build_itos
+def build_itos(vocab):
+    """Return a dict mapping each index 0..len(vocab)-1 to its character."""
+    # TODO: build an int-to-string lookup from the vocab list
+    dict={}
+    for i in range(len(vocab)):
+        dict[i]=vocab[i]
+    return dict
 
 # Step 4 - encode_char (not yet solved)
 # TODO: implement
